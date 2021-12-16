c0541fd1c84db0cd2e9eac032927742489a991c9
palasja <palasja@gmail.com>
2021-12-16 22:10:53 +0300

    SQL All

---
+++ 5/Create BD.sql
@@ -0,0 +1,53 @@
+USE master;
+GO
+IF DB_ID (N'Organization') IS NOT NULL
+DROP DATABASE Organization;
+GO
+CREATE DATABASE Organization;
+GO
+USE Organization;
+Go
+ DROP TABLE IF EXISTS Department;
+CREATE TABLE Department
+(
+Id int NOT NULL IDENTITY CONSTRAINT PK_Department PRIMARY KEY,
+Name varchar(20),
+Address varchar(100),
+)
+
+ DROP TABLE IF EXISTS Employee;
+CREATE TABLE Employee
+(
+Id int NOT NULL IDENTITY CONSTRAINT PK_Employee PRIMARY KEY,
+FullName varchar(20),
+Burthday date,
+)
+
+DROP TABLE IF EXISTS Job;
+CREATE TABLE Job
+(
+Id int NOT NULL IDENTITY CONSTRAINT PK_Job PRIMARY KEY,
+Name varchar(20),
+MinEarn smallmoney,
+)
+
+DROP TABLE IF EXISTS Career;
+CREATE TABLE Career
+(
+Id int NOT NULL IDENTITY CONSTRAINT PK_Career PRIMARY KEY,
+JobId int NOT NULL REFERENCES Job(Id),
+EmployeeId int NOT NULL REFERENCES Employee(Id),
+DepartmentId int NOT NULL REFERENCES Department(Id),
+Hire date NOT NULL,
+Fire date NULL
+)
+
+DROP TABLE IF EXISTS Salary;
+CREATE TABLE Salary
+(
+Id int NOT NULL IDENTITY CONSTRAINT PK_Salary PRIMARY KEY,
+EmployeeId int NOT NULL REFERENCES Employee(Id),
+Month tinyint NOT NULL CHECK(Month >0 AND Month < 13),
+Year smallint NOT NULL CHECK(Year >2002 AND Year < 2016),
+Earn money NOT NULL 
+)
--- 5/Query.sql
+++ 5/Query.sql
@@ -1,6 +1,6 @@
 USE Organization;
 GO
-/*
+
 Print '1A'
 Select * FROM Employee AS em; 
 
@@ -10,7 +10,7 @@ Select Name, MinEarn From Job Where MinEarn <= 1300;
 Print '1C'
 SELECT AVG(Earn) AS Average
 FROM Salary 
-WHERE Year = 2015 AND Month = 12
+WHERE Year = 2015 AND Month = 10
 
 Print '2A'
 Select Burthday, FullName From Employee Where Burthday = (
@@ -21,7 +21,12 @@ Select FullName From Employee Where Id IN (
 	Select EmployeeId From Salary
 	Where Month = 1 AND YEAR = 2015
 );
-
+Print '2C'
+SELECT S.EmployeeId, S.Earn, S.Month, S.Year FROM 
+(Select EmployeeId, Earn, Month, Year
+From Salary
+Where Month = 5 AND YEAR = 2015) AS tmp, Salary AS S 
+WHERE tmp.EmployeeId = S.EmployeeId AND S.Month < 5 AND tmp.YEAR = 2015 AND tmp.Earn > S.Earn
 Print '2D'
 SELECT Id, Name, (
 SELECT COUNT(*) FROM Career AS C
@@ -48,19 +53,11 @@ SELECT E.FullName, C.JobId, C.DepartmentId, [WorkMonths] = IIF(C.Fire IS NULL, (
 , (SELECT DATEDIFF(mm,C.Hire, C.Fire)))
 FROM Employee AS E JOIN Career AS C ON E.Id = C.EmployeeId
 Print '5A'
-SELECT TOP(10) * FROM Salary
-UPDATE Salary SET Earn = Earn * 1.5
-SELECT TOP(10) * FROM Salary
+SELECT TOP(10) * FROM Job
+UPDATE Job SET MinEarn = MinEarn * 1.5
+SELECT TOP(10) * FROM Job
 
 Print'5B'
-SELECT TOP(20) * FROM Salary
+SELECT * FROM Salary
 DELETE FROM Salary WHERE Year > 2014
-SELECT TOP(20) * FROM Salary
-*/
-
-/*SELECT * FROM Salary WHERE Month = 5 AND YEAR = 2015 AND Earn < ALL(
-Select Earn From Salary
-	Where Month = 5 AND YEAR = 2015
-)
-*/
-
+SELECT * FROM Salary
